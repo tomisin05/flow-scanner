61f2be1840d0d584adc2b05f00955a8d994ce5ae
Oluwatomisin John Badmus <155178644+tomisin05@users.noreply.github.com>
2025-01-13 23:54:01 -0500

    p

--- src/components/Tournaments.jsx
+++ src/components/Tournaments.jsx
@@ -178,7 +178,7 @@ export default function Tournaments() {
         </button> 
       </div>
 
-      {/* {error && (
+      {/* {error && ( 
         <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
           {error}
         </div>
