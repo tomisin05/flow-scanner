5cd3ef98afa1cd75ae084715566091e8c5dab2fd
Oluwatomisin John Badmus <155178644+tomisin05@users.noreply.github.com>
2025-01-14 15:44:16 -0500

    fixed the vite config build error

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-// import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
 
 // vite.config.js
-import { defineConfig } from 'vite';
-import { vitePlugin as remix } from "@remix-run/dev";
+// import { defineConfig } from 'vite';
+// import { vitePlugin as remix } from "@remix-run/dev";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    remix({
-      future: {
-        v7_startTransition: true,
-        v7_relativeSplatPath: true
-      }
-    })
-  ]
-});
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     remix({
+//       future: {
+//         v7_startTransition: true,
+//         v7_relativeSplatPath: true
+//       }
+//     })
+//   ]
+// });
